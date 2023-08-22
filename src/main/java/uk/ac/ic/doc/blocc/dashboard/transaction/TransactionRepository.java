0cda4177593a9c564df228ccda0854e6510d78f2
Tony Wu <tonywu3027@me.com>
2023-08-22 12:18:03 +0100

    fix(TransactionRepository): correct entity type to Transaction

--- src/main/java/uk/ac/ic/doc/blocc/dashboard/transaction/TransactionRepository.java
+++ src/main/java/uk/ac/ic/doc/blocc/dashboard/transaction/TransactionRepository.java
@@ -4,13 +4,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import uk.ac.ic.doc.blocc.dashboard.transaction.model.ApprovalTransaction;
 import uk.ac.ic.doc.blocc.dashboard.transaction.model.CompositeKey;
 import uk.ac.ic.doc.blocc.dashboard.transaction.model.Transaction;
 
 @Repository
 public interface TransactionRepository
-    extends JpaRepository<ApprovalTransaction, CompositeKey> {
+    extends JpaRepository<Transaction, CompositeKey> {
 
   @Query("SELECT tx FROM Transaction tx WHERE tx.key.containerNum = ?1")
   List<Transaction> findAllByContainerNum(int containerNum);
